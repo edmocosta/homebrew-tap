fffcd2509b11fca1fa867b938c89784cd0b1dfeb
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2023-06-21 19:44:05 +0200

    Update tuistash.rb

--- Formula/tuistash.rb
+++ Formula/tuistash.rb
@@ -3,7 +3,7 @@ class Tuistash < Formula
   homepage "https://github.com/edmocosta/tuistash"
   url "https://github.com/edmocosta/tuistash/releases/download/v0.1.1/tuistash-macos.tar.gz"
   sha256 "77d537cb32ceb1b7646f5271e083b4943c2ae69a42455509491ce5cd29e4cf3b"
-  version "0.1.1"
+  version "0.1.0"
   
   def install
     bin.install "tuistash"
