15f13a21e931771b4a96872404af28cdfa0d3761
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2023-06-23 10:17:07 +0200

    tuistash 0.1.2

--- Formula/tuistash.rb
+++ Formula/tuistash.rb
@@ -1,9 +1,9 @@
 class Tuistash < Formula
   desc "A terminal user interface for Logstash 🪵"
   homepage "https://github.com/edmocosta/tuistash"
-  url "https://github.com/edmocosta/tuistash/releases/download/v0.1.1/tuistash-macos.tar.gz"
-  sha256 "b98b2da0e5bb70b4cc7d104559a1270ccf54f5ea2a124b7c6d888c703d01e685"
-  version "0.1.1"
+  url "https://github.com/edmocosta/tuistash/releases/download/v0.1.2/tuistash-macos.tar.gz"
+  sha256 "00c8af0cd816d10a20ba56bc66620fb5ceb65f01aacdb9129f2a3b418e74f3ac"
+  version "0.1.2"
   
   def install
     bin.install "tuistash"
